ad201cbe10354b8a519c1453c7c6d4a956585d2d
DJ Bigelow <dj.bigelow@students.snow.edu>
2020-11-21 18:58:23 -0700

    Added delete functionality

--- src/components/ProjectDetails.tsx
+++ src/components/ProjectDetails.tsx
@@ -1,21 +1,22 @@
 import Axios from 'axios';
 import React, { FC, useEffect, useState } from 'react';
-import { Link, RouteComponentProps } from 'react-router-dom';
+import { Link, RouteComponentProps, useHistory } from 'react-router-dom';
 import { Project } from '../models/Project';
 import { ProjectRouteProp } from '../models/ProjectRouteProp';
+import { useAuth0 } from '@auth0/auth0-react';
+import { Button } from 'react-bootstrap';
 import ReactMarkdown from 'react-markdown';
 import './styles/ProjectDetails.css'
 import './styles/CodeHighlight.css'
-import { useAuth0 } from '@auth0/auth0-react';
-import { Button } from 'react-bootstrap';
 
 const apiUrl: string | undefined = process.env.REACT_APP_API_URL;
 
 
 export const ProjectDetails : FC<RouteComponentProps<ProjectRouteProp>> = ({match}) => {
     const [project, setProject] = useState<Project | null>(null);
-    const { user } = useAuth0();
-    
+    const { user, getAccessTokenSilently } = useAuth0();
+    const history = useHistory();
+
     useEffect(() => {
         const getProject = async () => {
             var response = await Axios.get(apiUrl + '/api/project/GetProject/' + match.params.slug)
@@ -26,6 +27,23 @@ export const ProjectDetails : FC<RouteComponentProps<ProjectRouteProp>> = ({matc
     }, [match.params.slug]);
     
 
+    const deleteProject = async () => {
+        const accessToken = await getAccessTokenSilently( {
+            audience: process.env.REACT_APP_AUDIENCE
+        });
+
+        console.log(accessToken)
+        await Axios.delete(
+            `${apiUrl}/api/project/deleteproject/${project?.id}`,          
+            {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            })
+        history.push('/projects') 
+    }
+
+
     if (user && user['https://schemas.dev-h2j88rmi.com/roles'].includes("Admin")) {
         return (
             <div>
@@ -34,7 +52,7 @@ export const ProjectDetails : FC<RouteComponentProps<ProjectRouteProp>> = ({matc
                 </div>
                 <div>
                     <Link to={`/projects/edit/${project?.slug}`}>Edit</Link>
-                    <Button >Delete</Button>
+                    <Button onClick={deleteProject}>Delete</Button>
                 </div>
             </div>
         )
